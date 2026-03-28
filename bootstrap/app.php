40e04714ad84a33dac128cb6b250666e7aa998a5
paufrancisco <francisco.paulo.pf@gmail.com>
2026-03-28 10:29:14 +0800

    TRY

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health:   '/up',
     )
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+    })
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
